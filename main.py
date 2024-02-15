bfe83996530eb6706ef3901df658291a047acf8e
Sumit <akoliyasumit@hotmail.com>
2024-02-15 16:50:46 +0000

    Signup and login page working

--- main.py
+++ main.py
@@ -1,9 +1,111 @@
-from flask import Flask, render_template
+from flask import Flask, flash ,render_template,request,redirect,session
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 app=Flask(__name__)
+app.config["SECRET_KEY"]='65b0b774279de460f1cc5c92'
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///ums.sqlite"
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=
+db=SQLAlchemy(app)
+bcrypt=Bcrypt(app)
 
-@app.route('/admin/')
+
+#User class
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    fname=db.Column(db.String(255), nullable=False)
+    lname=db.Column(db.String(255), nullable=False)
+    email=db.Column(db.String(255), nullable=False)
+    username=db.Column(db.String(255), nullable=False)
+    edu=db.Column(db.String(255), nullable=False)
+    password=db.Column(db.String(255), nullable=False)
+    status=db.Column(db.Integer,default=0, nullable=False)
+
+    def __repr__(self):
+        return f'User("{self.id}","{self.fname}","{self.lname}","{self.email}","{self.edu}","{self.username}","{self.status}")'
+
+#create table
+with app.app_context():
+    db.create_all()
+# db.create_all()
+
+#main index login
+@app.route('/')
+def index():
+    return render_template('index.html',title="")
+
+#admin login
+@app.route('/admin')
 def adminIndex():
     return render_template('admin/index.html',title="Admin login")
+
+#---------------------user area---------------------
+# user dashboard
+@app.route('/user/dashboard')
+def userDashboard():
+    if not session.get('user_id'):
+        return redirect('/user')
+    if session.get('user_id'):
+        id=session.get('user_id')
+        users=User().query.filter_by(id=id).first()
+        return render_template('user/dashboard.html',title="User Dashboard",users=users)
+
+@app.route('/user',methods=["POST","GET"])
+def userIndex():
+    if  session.get('user_id'):
+        return redirect('/user/dashboard')
+    if request.method=="POST":
+        # get the name of the field
+        email=request.form.get('email')
+        password=request.form.get('password')
+        # check user exist in this email or not
+        users=User().query.filter_by(email=email).first()
+        if users and bcrypt.check_password_hash(users.password,password):
+            # # check the admin approve your account are not
+            # is_approve=User.query.filter_by(id=users.id).first()
+            # # first return the is_approve:
+            # if is_approve.status == 0:
+            #     flash('Your Account is not approved by Admin','danger')
+            #     return redirect('/user/')
+            # else:
+                session['user_id']=users.id
+                session['username']=users.username
+                flash('Login Successfully','success')
+                return redirect('/user/dashboard')
+        else:
+            flash('Invalid Email and Password','danger')
+            return redirect('/user')
+    else:
+        return render_template('user/index.html',title="User Login")
+
+#user register
+@app.route('/user/signup',methods=["POST","GET"])
+def userSignup():
+    if request.method=='POST':
+        # get all input field name
+        fname=request.form.get('fname')
+        lname=request.form.get('lname')
+        email=request.form.get('email')
+        username=request.form.get('username')
+        edu=request.form.get('edu')
+        password=request.form.get('password')
+        # check all the field is filled are not
+        if fname =="" or lname=="" or email=="" or password=="" or username=="" or edu=="":
+            flash('Please fill all the field','danger')
+            return redirect('/user/signup')
+        else:
+            is_email=User().query.filter_by(email=email).first()
+            if is_email:
+                flash('Email already Exist','danger')
+                return redirect('/user/signup')
+            else:
+                hash_password=bcrypt.generate_password_hash(password,10)
+                user=User(fname=fname,lname=lname,email=email,password=hash_password,edu=edu,username=username)
+                db.session.add(user)
+                db.session.commit()
+                flash('Account Create Successfully Admin Will approve your account in 10 to 30 mint ','success')
+                return redirect('/user') 
+    else:
+        return render_template('user/signup.html',title="User Signup")
 
 
 if __name__=="__main__":
